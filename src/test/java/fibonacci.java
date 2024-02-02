ca1a7358dd3a5172a149852ec16ec8cfd5a44609
222hojun <sciboviator@gmail.com>
2024-02-02 12:17:25 +0900

    피보나치 수열을 만들어보자

---
+++ src/test/java/fibonacci.java
@@ -0,0 +1,87 @@
+public class fibonacci {
+
+    public static void main(String[] args) {
+
+        System.out.println("==================fibonacci======================================");
+
+        int x1;
+        int x2;
+        int n;
+
+        System.out.println("======fibonacci 3항=========================================");
+        x1=1; x2=1; n=1;
+
+        System.out.println("x"+ (n+2) + "번째 값은 : " + (x1+ x2));
+
+        int x3;
+        x3 = x2+x1;
+
+
+        System.out.println("=========fibonacci" + (n+3)+"항=================================");
+
+
+        x1= x2; x2= x3; x3= x2+x1; n=n+1;
+
+        System.out.println("x"+ (n+2) + "번째 값은 : " + (x1+ x2));
+
+        System.out.println("=========fibonacci" + (n+3)+"항=================================");
+
+
+        x1= x2; x2= x3; x3= x2+x1; n=n+1;
+
+        System.out.println("x"+ (n+2) + "번째 값은 : " + (x1+ x2)); System.out.println("=========fibonacci" + (n+3)+"항=================================");
+
+
+        x1= x2; x2= x3; x3= x2+x1; n=n+1;
+
+        System.out.println("x"+ (n+2) + "번째 값은 : " + (x1+ x2)); System.out.println("=========fibonacci" + (n+3)+"항=================================");
+
+
+        x1= x2; x2= x3; x3= x2+x1; n=n+1;
+
+        System.out.println("x"+ (n+2) + "번째 값은 : " + (x1+ x2)); System.out.println("=========fibonacci" + (n+3)+"항=================================");
+
+
+        x1= x2; x2= x3; x3= x2+x1; n=n+1;
+
+        System.out.println("x"+ (n+2) + "번째 값은 : " + (x1+ x2)); System.out.println("=========fibonacci" + (n+3)+"항=================================");
+
+
+        x1= x2; x2= x3; x3= x2+x1; n=n+1;
+
+        System.out.println("x"+ (n+2) + "번째 값은 : " + (x1+ x2)); System.out.println("=========fibonacci" + (n+3)+"항=================================");
+
+
+        x1= x2; x2= x3; x3= x2+x1; n=n+1;
+
+        System.out.println("x"+ (n+2) + "번째 값은 : " + (x1+ x2)); System.out.println("=========fibonacci" + (n+3)+"항=================================");
+
+
+        x1= x2; x2= x3; x3= x2+x1; n=n+1;
+
+        System.out.println("x"+ (n+2) + "번째 값은 : " + (x1+ x2)); System.out.println("=========fibonacci" + (n+3)+"항=================================");
+
+
+        x1= x2; x2= x3; x3= x2+x1; n=n+1;
+
+        System.out.println("x"+ (n+2) + "번째 값은 : " + (x1+ x2)); System.out.println("=========fibonacci" + (n+3)+"항=================================");
+
+
+        x1= x2; x2= x3; x3= x2+x1; n=n+1;
+
+        System.out.println("x"+ (n+2) + "번째 값은 : " + (x1+ x2));
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
